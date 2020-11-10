64b9a69c31ef145623a5e9efc3cedb7083134898
ektha <ektha3869@naver.com>
2020-11-11 03:39:30 +0900

    [POST] 1111

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,9 @@ import { Link } from "gatsby"
 
 const Header = () => ( 
   <div>
-    <Link style={{ margin:5, textDecoration:"none", fontWeight:"bold", color:"#F4B8C6" }} to="/blog">Blog</Link>
     <Link style={{ margin:5, textDecoration:"none", fontWeight:"bold", color:"#F4B8C6" }} to="/algorithm">Algorithm</Link>
+    <Link style={{ margin:5, textDecoration:"none", fontWeight:"bold", color:"#F4B8C6" }} to="/blog">Blog</Link>
+    <Link style={{ margin:5, textDecoration:"none", fontWeight:"bold", color:"#F4B8C6" }} to="/study">Study</Link>
     <Link style={{ margin:5, textDecoration:"none", fontWeight:"bold", color:"#F4B8C6" }} to="/todo">Todo</Link>
     <Link style={{ margin:5, textDecoration:"none", fontWeight:"bold", color:"#F4B8C6" }} to="/about">About</Link>
   </div>
