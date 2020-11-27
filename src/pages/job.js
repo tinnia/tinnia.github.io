8e46de44ff84c1cd9e2e9d704b9dd11fc5d0e5a4
ektha <ektha3869@naver.com>
2020-11-27 23:03:53 +0900

    [1127] POST

---
+++ src/pages/job.js
@@ -0,0 +1,73 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const JobCategory = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata?.title || `Title`
+  const posts = data.allMarkdownRemark.nodes.filter(post => post.frontmatter.category === "interview")
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="All posts" />
+      <ol style={{ listStyle: `none` }}>
+        {posts.map(post => {
+          const title = post.frontmatter.title || post.fields.slug
+          return (
+            <li key={post.fields.slug}>
+              <article
+                  className="post-list-item"
+                  itemScope
+                  itemType="http://schema.org/Article"
+              >
+                <header>
+                  <h2 style={{ marginBottom:0 }}>
+                      <Link style={{ color:"#8C749F" }} to={post.fields.slug} itemProp="url">
+                      <span itemProp="headline">{title}</span>
+                      </Link>
+                  </h2>
+                  <small>{post.frontmatter.date}</small>
+                </header>
+                <section>
+                  <p
+                      dangerouslySetInnerHTML={{
+                      __html: post.frontmatter.description || post.excerpt,
+                      }}
+                      itemProp="description"
+                  />
+                </section>
+              </article>
+            </li>
+          )
+        })}
+      </ol>
+    </Layout>
+  )
+}
+
+export default JobCategory
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      nodes {
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "YYYY/MMM/DD")
+          title
+          category
+          description
+        }
+      }
+    }
+  }
+`
